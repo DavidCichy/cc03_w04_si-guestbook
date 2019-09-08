d01962373655099abb80042a91c319bd67b2d718
David <cichy.dawid@gmail.com>
2019-09-08 13:12:48 +0200

    Maven template

---
+++ src/main/java/Hello.java
@@ -0,0 +1,15 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class Hello implements HttpHandler {
+    public void handle(HttpExchange httpExchange) throws IOException {
+        String response = "Hello World!";
+        httpExchange.sendResponseHeaders(200, response.length());
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
+}
